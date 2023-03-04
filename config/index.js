ce9c5ae116599e9f9bc946cfb770656851fd960a
Chenuka Sumanasekara <chenuka@swivelgroup.com.au>
2023-03-04 10:35:21 +0530

    MongoDB URL location changed

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
     PORT: process.env.PORT || 5000,
-    MONGO_URL: process.env.MONGO_URL,
+    MONGO_URL: process.env.MONGO_URL || 'mongodb+srv://user:dev123@development-jlc.ceu2qcl.mongodb.net/shop?retryWrites=true&w=majority',
     ENV: process.env.ENV || 'development',
     //ENV: process.env.ENV || 'production'
 }
