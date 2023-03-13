093ee3e0a530a2d40194ef4d9402d53097bb6593
Chenuka Sumanasekara <chenuka@swivelgroup.com.au>
2023-03-14 00:06:40 +0530

    Swagger is added

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,7 +2,37 @@ const router = require("express").Router();
 const authController = require("../controllers/auth.controllers");
 const { userDataValidation } = require("../middlewares/validations");
 
+/**
+ * @swagger
+ * /api/auth/register:
+ *  post:
+ *     tags: 
+ *        - Auth
+ *     description: Register user
+ *     responses:
+ *          '200': 
+ *              description: User registered successfully
+ *          '400':
+ *              description: You've entered wrong details
+ *          '500':
+ *              description: Internal server error
+ */
 router.post("/register", userDataValidation, authController.registerUser);
+/**
+ * @swagger
+ * /api/auth/login:
+ *  post:
+ *     tags: 
+ *        - Auth
+ *     description: Login user
+ *     responses:
+ *          '200': 
+ *              description: User logged in successfully
+ *          '401':
+ *              description: You've entered wrong credentials
+ *          '500':
+ *              description: Internal server error
+ */
 router.post("/login", authController.loginUser);
 
 module.exports = router;
--- routes/stripe.routes.js
+++ routes/stripe.routes.js
@@ -1,8 +1,38 @@
 const router = require("express").Router();
 const stripeController = require("../controllers/stripe.controllers");
-const { verifyTokenAndAuthorization, verifyTokenAndAdmin, verifyToken } = require("../middlewares/verifyToken");
+const { verifyTokenAndAdmin, verifyToken } = require("../middlewares/verifyToken");
 
+/**
+ * @swagger
+ * /api/payments/checkout:
+ *  post:
+ *     tags: 
+ *        - Payment
+ *     description: Process payment
+ *     responses:
+ *          '200': 
+ *              description: Payment is processed successfully
+ *          '403':
+ *              description: Token is invalid
+ *          '500':
+ *              description: Internal server error
+ */
 router.post("/checkout", verifyToken, stripeController.processPayment);
+/**
+ * @swagger
+ * /api/payments:
+ *  get:
+ *     tags: 
+ *        - Payment
+ *     description: Get all payments
+ *     responses:
+ *          '200': 
+ *              description: All payments are found/empty
+ *          '401':
+ *              description: You are not allowed
+ *          '500':
+ *              description: Internal server error
+ */
 router.get("/", verifyTokenAndAdmin, stripeController.getAllPayments)
 
 module.exports = router;
